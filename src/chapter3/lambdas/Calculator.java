453ec0c0f527a483d49f1c1334eff1f90a3454d3
moonycode <chl93m@naver.com>
2025-07-16 10:29:23 +0900

    Initial commit

---
+++ src/chapter3/lambdas/Calculator.java
@@ -0,0 +1,6 @@
+package chapter3.exceptions.lambdas;
+
+public interface Calculator {
+    int sum(int a, int b);
+
+}
---
+++ src/chapter3/lambdas/Main.java
@@ -0,0 +1,22 @@
+package chapter3.exceptions.lambdas;
+
+public class Main {
+    public static void main(String[] args) {
+//        Calculator1 calculator1 = new Calculator1();
+//        int ret1= calculator1.sum(2,3);
+//        System.out.println("ret1 = " + ret1);
+//
+        // 인터페이스를 활용한 익명클래스 만들기
+        Calculator calculator1 = new Calculator() {
+
+
+            @Override
+            public int sum(int a, int b) {
+                return a+b;
+            }
+        };
+
+        int ret1 = calculator1.sum(2,3);
+        System.out.println("ret1 = " + ret1);
+    }
+}
